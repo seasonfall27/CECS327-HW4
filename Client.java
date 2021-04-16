9a36c1d765bbf3900934fa47718ec68ec4d21c8e
Autumn Nguyen <38737498+seasonfall27@users.noreply.github.com>
2021-04-15 17:23:15 -0700

    Initial Client Server java file commits

---
+++ Client.java
@@ -0,0 +1,12 @@
+import java.net.*;
+import java.util.Scanner;
+import java.io.*;
+
+public class Client{
+
+    }
+
+    public static void main(String args[]){
+        System.out.println("Beginning echo client...");
+    }
+}
---
+++ Server.java
@@ -0,0 +1,8 @@
+import java.net.*;
+import java.io.*;
+
+public class Server{
+
+    public static void main(String args[]){
+    }
+}
