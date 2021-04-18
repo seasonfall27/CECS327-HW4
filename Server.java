ec49a2b6b38f07efb13db097ca5fe5bfbc8745c3
Autumn Nguyen <38737498+seasonfall27@users.noreply.github.com>
2021-04-17 23:14:17 -0700

    Update Server.java

--- Server.java
+++ Server.java
@@ -4,5 +4,26 @@ import java.io.*;
 public class Server{
 
     public static void main(String args[]){
+        System.out.println("Beginning echo server...");
+
+        // Instantiate Server socket instance
+        // Used by server to listen to client request
+        try(ServerSocket echoServer = new ServerSocket(8000)){
+            Socket clientSocket = echoServer.accept();
+            System.out.println("Connected to client!");
+            
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            PrintWriter output = new PrintWriter(clientSocket.getOutputStream(), true); 
+
+            // Loops until the message is null
+            String message;
+            while((message = bufferedReader.readLine()) != null){
+                System.out.println("Server: " + message);
+                // Print back to client
+                output.println(message);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
